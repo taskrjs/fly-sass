702421bafa24ea5a8e05890e57542d2a7585d128
Tomoyuki Kashiro <ksrtmyk@gmail.com>
2015-08-23 14:20:50 +0900

    add tests

---
+++ Flyfile.js
@@ -0,0 +1,11 @@
+export default function* () {
+  yield this.clear("test/style.css")
+  yield this
+    .source("test/src/style.scss")
+    .sass({
+      outputStyle: "compressed",
+      includePaths: ["test/src/imports"],
+      sourceMap: true
+    })
+    .target("test")
+}
--- test/index.js
+++
@@ -1,6 +0,0 @@
-const test = require("tape").test
-
-test("fly-node-sass", function (t) {
-  t.ok(true, "ok")
-  t.end()
-})
