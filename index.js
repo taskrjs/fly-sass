c06302c6c046af85c63a95642a48a991a1ef1599
Romain Maneschi <romain@maneschi.fr>
2015-08-30 16:18:52 +0200

    add file option to debug more easily

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = function (debug) {
   this.filter("sass", (data, options) => {
     return this.defer(sass)(
       assign({ data: data.toString() },
-      assign({ outFile: options.sourceMap ? "." : "" }, options))
+      assign({ outFile: options.sourceMap ? "." : "", file: options.filename }, options))
     ).then((result) => assign({ ext: ".css" }, result))
   })
 }
