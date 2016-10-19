e75796b2a79b1fd43722178a9f0a24e36d9b6aa2
Luke Edwards <luke.edwards05@gmail.com>
2016-10-19 20:45:12 +0200

    add sourcemap tests

--- test/index.js
+++ test/index.js
@@ -10,7 +10,7 @@ const tmp = join(__dirname, 'tmp');
 const expect = '.file__c{visibility:hidden}.file__b{display:block}.file__a{display:none}.main{display:block}\n';
 
 test('fly-sass', t => {
-	t.plan(3);
+	t.plan(7);
 
 	const fly = new Fly({
 		plugins: [{
@@ -18,6 +18,7 @@ test('fly-sass', t => {
 		}],
 		tasks: {
 			a: function * () {
+				const map = `${tmp}/out.css.map`;
 				const src = `${dir}/style.sass`;
 				const tar = `${tmp}/style.css`;
 
@@ -26,6 +27,18 @@ test('fly-sass', t => {
 
 				yield this.source(src).sass({outputStyle: 'compressed'}).target(tmp);
 				t.equal(yield this.$.read(tar, 'utf8'), expect, 'resolve multi-level imports && types!');
+
+				yield this.source(src).sass({sourceMap: map}).target(tmp);
+				const arr1 = yield this.$.expand(`${tmp}/*`);
+				t.equal(arr1.length, 2, 'via `sourceMap`; create a source map');
+				t.ok(yield this.$.find(map), 'via `sourceMap`; create a source map with custom name');
+				yield this.clear(tmp);
+
+				yield this.source(src).sass({sourceMap: true, outFile: map}).target(tmp);
+				const arr2 = yield this.$.expand(`${tmp}/*`);
+				t.equal(arr2.length, 2, 'via `sourceMap + outFile`; create a source map');
+				t.ok(yield this.$.find(map), 'via `sourceMap + outFile`; create a source map with custom name');
+				yield this.clear(tmp);
 			}
 		}
 	});
